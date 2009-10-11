1cc5a2452d3cebf22a41afe2db672846fba2fcf1
Mark Paschal <markpasc@markpasc.org>
2009-10-10 23:03:32 -0700

    Mark extra scripts executable

--- install-extras.sh
+++ install-extras.sh
@@ -3,7 +3,7 @@
 # lns -- a friendly program for making symbolic links by Sean M. Burke
 # http://interglacial.com/~sburke/pub/lns.html
 curl -o ~/bin/lns http://interglacial.com/~sburke/pub/lns
-chmod +x lns
+chmod +x ~/bin/lns
 
 # ack -- grep-like text finder by Andy Lester
 # http://betterthangrep.com/
@@ -11,4 +11,5 @@ chmod +x lns
 # Would just curl it but want to fix this bug in 1.90.
 #    curl -o ~/bin/ack http://betterthangrep.com/ack-standalone
 curl http://betterthangrep.com/ack-standalone | sed 's/return unless App::Ack::output_to_pipe();/return if App::Ack::output_to_pipe();/' > ~/bin/ack
+chmod +x ~/bin/ack
 
