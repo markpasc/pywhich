1b8b20ca1360d812e54fa3cc66805ea9ef8a47f6
Mark Paschal <markpasc@markpasc.org>
2013-03-18 14:15:39 -0400

    Remove module from setup.py
    and add classifiers etc.

--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ setup(
     name='pywhich',
     version='1.1',
     description='Find where Python modules are installed in the current Python',
-    py_modules=['pywhich'],
     scripts=['bin/pywhich'],
+    author='Mark Paschal',
+    author_email='markpasc@markpasc.org',
+    url='https://github.com/markpasc/pywhich',
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
 )
